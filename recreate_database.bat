5b52c2f12a7acaaa4db6d53318fe1d271bd11e2a
Ahmed Imtiaz <ahmedimtqureshi@gmail.com>
2025-09-04 12:21:55 -0700

    Cleaned repo: ignore models, datasets, bat files

--- recreate_database.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Recreating database...
-
-mysql -u root -pWarmsi@123 -e "DROP DATABASE IF EXISTS bus_routes_db;"
-mysql -u root -pWarmsi@123 -e "CREATE DATABASE IF NOT EXISTS bus_routes_db;"
-mysql -u root -pWarmsi@123 bus_routes_db < setup_database.sql
-
-echo Database recreation completed!
-pause 
